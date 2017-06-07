ab6ee6656daa63fc4f1f8c73bed109170d643986
gugugi_lin <lily09290110@gmail.com>
2017-06-07 22:37:36 +0800

    v2_7

--- exe.sh
+++ exe.sh
@@ -1,8 +1,21 @@
 name=$1
 IFS='/' read -r -a array <<< "$name"
 len=${#array[@]}
+if [ $len -gt 0 ];then
 IFS='.' read -r -a array <<< "${array[$len-1]}"
-./a1.out $name > "${array[0]}.jasm"
-./javaa/javaa "${array[0]}.jasm"
-java ${array[0]}
-
+len=${#array[@]}
+echo ${array[1]}
+if [ $len -gt 1 ];then
+    if [ "${array[1]}"=="go" ];then
+        ./a1.out $name > "${array[0]}.jasm"
+        ./javaa/javaa "${array[0]}.jasm"
+        java ${array[0]}
+    else
+        echo "must be a go file"
+    fi
+else
+    echo "argument error"
+fi
+else
+    echo "argument error"
+fi
